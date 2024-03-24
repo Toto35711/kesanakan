6292d5215e822d764a9c98de2d12d814a97f53a7
Toto Budi Trapsilo <toto2441999@gmail.com>
2024-03-24 11:57:31 +0700

    initial commit

---
+++ src/main/java/id/tototrapsilo/kesanakan/KesanakanApplication.java
@@ -0,0 +1,13 @@
+package id.tototrapsilo.kesanakan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KesanakanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KesanakanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/tototrapsilo/kesanakan/KesanakanApplicationTests.java
@@ -0,0 +1,13 @@
+package id.tototrapsilo.kesanakan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KesanakanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
