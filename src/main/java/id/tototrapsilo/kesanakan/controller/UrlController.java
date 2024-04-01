ccfa2dbf42cd11b9acb885d336feaa975abd8782
Toto Budi Trapsilo <toto.trapsilo@nicepay.co.id>
2024-04-01 09:46:41 +0700

    fix(urlController): redirect view

--- src/main/java/id/tototrapsilo/kesanakan/controller/UrlController.java
+++ src/main/java/id/tototrapsilo/kesanakan/controller/UrlController.java
@@ -6,15 +6,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/url")
@@ -26,7 +23,7 @@ public class UrlController {
     @RequestMapping(value = "/{key}", method=RequestMethod.GET)
     public RedirectView redirectUrl(@PathVariable String key, HttpServletRequest request, HttpServletResponse response) throws IOException, URISyntaxException, Exception {
         String redirectUrlString = urlService.getUrlByKey(key);
-        if (redirectUrlString == null){
+        if (redirectUrlString == null) {
             redirectUrlString = "https://www.tototrapsilo.id/404";
         }
         RedirectView redirectView = new RedirectView();
